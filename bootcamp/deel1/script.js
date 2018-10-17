a2ff21d15a67533f42ec524a3188290d4287cd96
Thomas van den Berg <30773643+kipkop4@users.noreply.github.com>
2018-10-17 11:01:23 +0200

    Add files via upload

---
+++ bootcamp/deel1/script.js
@@ -0,0 +1,12 @@
+var klas = "MD2B";
+var name = "Thomas van den Berg";
+
+
+
+ naam  = document.getElementsByClassName('student');
+
+for (var i = 0; i < naam.length; i++) {
+  naam  [i].innerText = name;
+}
+
+   document.getElementById('klas').innerText = klas;
